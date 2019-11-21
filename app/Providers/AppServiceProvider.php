2e62314aeacfd5e60dde28a0c92f267a2708d952
Sebastian <sibastian2@gmail.com>
2019-11-21 15:59:18 +0100

    Add service provider repositoy

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -16,6 +16,34 @@ class AppServiceProvider extends ServiceProvider
         $this->app->bind(
             'App\Repositories\Interfaces\ProjectRepositoryInterface',
             'App\Repositories\ProjectRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\AbsenceRepositoryInterface',
+            'App\Repositories\AbsenceRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\AgreementRepositoryInterface',
+            'App\Repositories\AgreementRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\AssortmentRepositoryInterface',
+            'App\Repositories\AssortmentRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\ContractorRepositoryInterface',
+            'App\Repositories\ContractorRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\EmployeeRepositoryInterface',
+            'App\Repositories\EmployeeRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\OrderRepositoryInterface',
+            'App\Repositories\OrderRepository');
+
+        $this->app->bind(
+            'App\Repositories\Interfaces\WorkplaceRepositoryInterface',
+            'App\Repositories\WorkplaceRepository');
     }
 
     /**
